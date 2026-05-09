afcb7a949a8db5774c5bf36a7ca0362e8ae4e3e2
User <anweshmidya.silversky@gmail.com>
2026-05-09 14:30:26 +0530

    Added jwt-passport, fixed month issue in expences dashboard

--- src/expences/expences.module.ts
+++ src/expences/expences.module.ts
@@ -5,11 +5,13 @@ import { UsersModule } from 'src/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Expences } from './expences.entity';
 import { TokenModule } from '../auth/jwt/token.module';
+import { AuthModule } from '../auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Expences])
-    ,UsersModule, TokenModule],
+    TypeOrmModule.forFeature([Expences]),
+    UsersModule, TokenModule],
   providers: [ExpencesService],
   controllers: [ExpencesController]
 })
